f19c69d173cf91cc7ffaf0c9494192545bff7c83
catmac25 <bestbillu25@gmail.com>
2025-05-14 19:32:11 +0530

    new comit

--- src/constants/experience.js
+++ src/constants/experience.js
@@ -9,12 +9,4 @@ export const workExperiences = [
       title: "Single Day Hackathon,for Web Design. I along with my team members made 'SpaceTrailzz' - a Space Newsletter website for the aliens. TechStack used included HTML, CSS, JS, React, 3D design using Spline",
       icon: img1
     },
-    {
-      id: 2,
-      name: 'Internship - InternPE',
-      pos: 'Web Development Intern',
-      duration: 'March 2023',
-      title: "I made FullStack websites using HTML, CSS, JS, React, Node and ExpressJS, MongoDB. It was a remote internship.",
-      icon: img2
-    }
   ];
